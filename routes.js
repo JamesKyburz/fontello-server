c032291d42532d0ad2b9a419853046dc6aba861d
James Kyburz <james.kyburz@gmail.com>
2016-01-27 15:30:10 +0100

    cache headers

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ var fontelloMount = require('fontello-mount')
 var store = require('./store')
 var leveldb = require('leveldb-mount').routes()
 var fs = require('fs')
+var crypto = require('crypto')
 module.exports = routes
 
 function routes (router) {
@@ -14,17 +15,31 @@ function routes (router) {
       if (err) return this.notFound(q, r)
       fontello(config, (err, route, cache) => {
         if (err) return this.errorReply(q, r)
-        q.url = q.url.replace('/' + params.hash, '')
-        route(q, r, this.notFound.bind(this, q, r))
+        var key = q.url.replace('/' + params.hash, '').split('?')[0]
+        var etag = createEtag(params.hash, key)
+        if (q.headers['if-none-match'] === etag) {
+          r.writeHead(304)
+          return r.end()
+        }
+        var file = cache.files[key]
+        if (!file) return this.notFound(q, r)
+        var data = new Buffer(file)
+        r.writeHead(200, {
+          'content-type': cache.types[key],
+          'etag': etag,
+          'last-modified': new Date().toGMTString(),
+          'content-length': data.length
+        })
+        r.end(data)
       })
     })
   })
 
   router.set('/upload/config.json', (q, r) => {
     this.jsonBody(q, r, config => {
-      fontello(config, (err, route, cache) => {
+      fontello(config, (err, route, cache, store) => {
         if (err) return this.errorReply(q, r, err)
-        r.end(cache.hash())
+        r.end(store.hash())
       })
     })
   })
@@ -35,9 +50,14 @@ function routes (router) {
 }
 
 function fontello (config, cb) {
-  var cache = store(config)
-  fontelloMount(config, { cache: cache }, (err, route) => {
+  var cacheStore = store(config)
+  fontelloMount(config, { cache: cacheStore }, (err, route, cache) => {
     if (err) return cb(err)
-    cb(null, route, cache)
+    cb(null, route, cache, cacheStore)
   })
+}
+
+function createEtag (hash, key) {
+  var sha1 = crypto.createHash('sha1')
+  return sha1.update(hash + key).digest('hex')
 }
